a65d29a5b36037643bbe594242b0320062198a26
Jun Gu <2dxgujun@gmail.com>
2015-04-14 16:41:23 +0800

    Fixed backspace on Google Keyboard.

--- library/src/main/java/me/gujun/android/taggroup/TagGroup.java
+++ library/src/main/java/me/gujun/android/taggroup/TagGroup.java
@@ -24,6 +24,8 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
+import android.view.inputmethod.InputConnectionWrapper;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -1093,6 +1095,32 @@ public class TagGroup extends ViewGroup {
                 }
             }
             return true;
+        }
+
+        @Override
+        public InputConnection onCreateInputConnection(EditorInfo outAttrs) {
+            return new ZanyInputConnection(super.onCreateInputConnection(outAttrs), true);
+        }
+
+        /**
+         * Solve edit text delete(backspace) key detect, see<a href="http://stackoverflow.com/a/14561345/3790554">
+         * Android: Backspace in WebView/BaseInputConnection</a>
+         */
+        private class ZanyInputConnection extends InputConnectionWrapper {
+            public ZanyInputConnection(android.view.inputmethod.InputConnection target, boolean mutable) {
+                super(target, mutable);
+            }
+
+            @Override
+            public boolean deleteSurroundingText(int beforeLength, int afterLength) {
+                // magic: in latest Android, deleteSurroundingText(1, 0) will be called for backspace
+                if (beforeLength == 1 && afterLength == 0) {
+                    // backspace
+                    return sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DEL))
+                            && sendKeyEvent(new KeyEvent(KeyEvent.ACTION_UP, KeyEvent.KEYCODE_DEL));
+                }
+                return super.deleteSurroundingText(beforeLength, afterLength);
+            }
         }
     }
 }
