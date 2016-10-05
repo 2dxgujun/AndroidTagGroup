f5258150f672b89a25457b47bb5069ccd3a8c717
Tuan Dinh <leonacky@gmail.com>
2016-10-05 18:01:55 +0700

    add TagItem

---
+++ library/src/main/java/me/gujun/android/taggroup/TagItem.java
@@ -0,0 +1,17 @@
+package me.gujun.android.taggroup;
+
+/**
+ * Created by leonacky on 10/5/16.
+ */
+
+public class TagItem {
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
